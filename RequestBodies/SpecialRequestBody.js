171fe1c676f60f3a53bbd02c4f6eaed007936c79
shwetang prajapati <shwetang.p@devtrust.biz>
2023-12-26 12:50:30 +0530

    Added  collective notes flow

--- RequestBodies/SpecialRequestBody.js
+++ RequestBodies/SpecialRequestBody.js
@@ -1,38 +1,38 @@
 const success = {
     "button_id" : "Rush",
     "order_id" : "OrderNY-ABC_02",
-    "order":{"note":{"transactionId":118359,"userEmail":"title@bluelandtitle.com","read":false}}
+    "order":{"note":{"transactionId":118359,"userEmail":"sverma@actiontitleresearch.com","read":false}}
 }   
 const buttonIdMissing = {
     "button_id" : "",
     "order_id" : "OrderNY-ABC_02",
-    "order":{"note":{"transactionId":118359,"userEmail":"title@bluelandtitle.com","read":false}}
+    "order":{"note":{"transactionId":118359,"userEmail":"sverma@actiontitleresearch.com","read":false}}
 }  
 const buttonIdWrong = {
     "button_id" : "abc",
     "order_id" : "OrderNY-ABC_02",
-    "order":{"note":{"transactionId":118359,"userEmail":"title@bluelandtitle.com","read":false}}
+    "order":{"note":{"transactionId":118359,"userEmail":"sverma@actiontitleresearch.com","read":false}}
 }  
 
 const WithoutButtonID = {
     "order_id" : "OrderNY-ABC_02",
-    "order":{"note":{"transactionId":118359,"userEmail":"title@bluelandtitle.com","read":false}}
+    "order":{"note":{"transactionId":118359,"userEmail":"sverma@actiontitleresearch.com","read":false}}
 }  
 
 const order_idMissing = {
     "button_id" : "Rush",
     "order_id" : "",
-    "order":{"note":{"transactionId":118359,"userEmail":"title@bluelandtitle.com","read":false}}
+    "order":{"note":{"transactionId":118359,"userEmail":"sverma@actiontitleresearch.com","read":false}}
 }
 
 const order_idWrong = {
     "button_id" : "Rush",
     "order_id" : "sfdfdsfedsfdsf",
-    "order":{"note":{"transactionId":118359,"userEmail":"title@bluelandtitle.com","read":false}}
+    "order":{"note":{"transactionId":118359,"userEmail":"sverma@actiontitleresearch.com","read":false}}
 }
 const Withoutorder_id = {
     "button_id" : "Rush",
-    "order":{"note":{"transactionId":118359,"userEmail":"title@bluelandtitle.com","read":false}}
+    "order":{"note":{"transactionId":118359,"userEmail":"sverma@actiontitleresearch.com","read":false}}
 }
 const orderMissing = {
     "button_id" : "Rush",
